f4ddfe5b5d6d87fe80c608fe24d77fc9b1256f33
Ryan Yang <yangryan0@gmail.com>
2018-07-10 06:12:00 +0000

    parameter_test

---
+++ tests/test_parameter_server/LogisticSparseTaskS3Test.cpp
@@ -0,0 +1,166 @@
+#include <Tasks.h>
+
+#include "Serializers.h"
+#include "Utils.h"
+#include "S3SparseIterator.h"
+#include "PSSparseServerInterface.h"
+
+#include <pthread.h>
+
+#undef DEBUG
+
+namespace cirrus {
+
+void LogisticSparseTaskS3::push_gradient(LRSparseGradient* lrg) {
+#ifdef DEBUG
+  auto before_push_us = get_time_us();
+  std::cout << "Publishing gradients" << std::endl;
+#endif
+  psint->send_lr_gradient(*lrg);
+#ifdef DEBUG
+  std::cout << "Published gradients!" << std::endl;
+  auto elapsed_push_us = get_time_us() - before_push_us;
+  static uint64_t before = 0;
+  if (before == 0)
+    before = get_time_us();
+  auto now = get_time_us();
+  std::cout << "[WORKER] "
+      << "Worker task published gradient"
+      << " with version: " << lrg->getVersion()
+      << " at time (us): " << get_time_us()
+      << " took(us): " << elapsed_push_us
+      << " bw(MB/s): " << std::fixed <<
+         (1.0 * lrg->getSerializedSize() / elapsed_push_us / 1024 / 1024 * 1000 * 1000)
+      << " since last(us): " << (now - before)
+      << "\n";
+  before = now;
+#endif
+}
+
+// get samples and labels data
+bool LogisticSparseTaskS3::get_dataset_minibatch(
+    std::unique_ptr<SparseDataset>& dataset,
+    SparseDataset& dataset_train) {
+#ifdef DEBUG
+  auto start = get_time_us();
+#endif
+
+  SparseDataset minibatch = dataset_train.random_sample(config.get_minibatch_size());
+#ifdef DEBUG
+  auto finish1 = get_time_us();
+#endif
+  dataset.reset(minibatch); // this takes 11 us
+
+#ifdef DEBUG
+  auto finish2 = get_time_us();
+  double bw = 1.0 * dataset->getSizeBytes() /
+    (finish2-start) * 1000.0 * 1000 / 1024 / 1024;
+  std::cout << "[WORKER] Get Sample Elapsed (S3) "
+    << " minibatch size: " << config.get_minibatch_size()
+    << " part1(us): " << (finish1 - start)
+    << " part2(us): " << (finish2 - finish1)
+    << " BW (MB/s): " << bw
+    << " at time: " << get_time_us()
+    << "\n";
+#endif
+  return true;
+}
+
+void LogisticSparseTaskS3::run(const Configuration& config, int worker) {
+  std::cout << "Starting LogisticSparseTaskS3"
+    << std::endl;
+  uint64_t num_s3_batches = config.get_limit_samples() / config.get_s3_size();
+  this->config = config;
+
+  psint = new PSSparseServerInterface(ps_ip, ps_port);
+  sparse_model_get = std::make_unique<SparseModelGet>(ps_ip, ps_port);
+  
+  std::cout << "[WORKER] " << "num s3 batches: " << num_s3_batches
+    << std::endl;
+  wait_for_start(worker, nworkers);
+
+  // Create iterator that goes from 0 to num_s3_batches
+  auto train_range = config.get_train_range();
+
+  std::cout << "[WORKER] starting loop" << std::endl;
+
+  uint64_t version = 1;
+  SparseLRModel model(1 << config.get_model_bits());
+
+  bool printed_rate = false;
+  int count = 0;
+  InputReader input;
+  SparseDataset dataset_train = input.read_input_criteo_kaggle_sparse(
+      "tests/test_data/train_lr.csv", ",", config);
+  auto start_time = get_time_ms();
+  while (1) {
+    // get data, labels and model
+#ifdef DEBUG
+    std::cout << get_time_us() << " [WORKER] running phase 1" << std::endl;
+#endif
+    std::unique_ptr<SparseDataset> dataset;
+    if (!get_dataset_minibatch(dataset, dataset_train)) {
+      continue;
+    }
+#ifdef DEBUG
+    std::cout << get_time_us() << " [WORKER] phase 1 done. Getting the model" << std::endl;
+    //dataset->check();
+    //dataset->print_info();
+    auto now = get_time_us();
+#endif
+    // compute mini batch gradient
+    std::unique_ptr<ModelGradient> gradient;
+
+    // we get the model subset with just the right amount of weights
+    sparse_model_get->get_new_model_inplace(*dataset, model, config);
+
+#ifdef DEBUG
+    std::cout << "get model elapsed(us): " << get_time_us() - now << std::endl;
+    std::cout << "Checking model" << std::endl;
+    //model.check();
+    std::cout << "Computing gradient" << "\n";
+    now = get_time_us();
+#endif
+
+    try {
+      gradient = model.minibatch_grad_sparse(*dataset, config);
+    } catch(const std::runtime_error& e) {
+      std::cout << "Error. " << e.what() << std::endl;
+      exit(-1);
+    } catch(...) {
+      std::cout << "There was an error computing the gradient" << std::endl;
+      exit(-1);
+    }
+#ifdef DEBUG
+    auto elapsed_us = get_time_us() - now;
+    std::cout << "[WORKER] Gradient compute time (us): " << elapsed_us
+      << " at time: " << get_time_us()
+      << " version " << version << "\n";
+#endif
+    gradient->setVersion(version++);
+
+    try {
+      LRSparseGradient* lrg = dynamic_cast<LRSparseGradient*>(gradient.get());
+      push_gradient(lrg);
+    } catch(...) {
+      std::cout << "[WORKER] "
+        << "Worker task error doing put of gradient" << "\n";
+      exit(-1);
+    }
+#ifdef DEBUG
+    std::cout << get_time_us() << " [WORKER] Sent gradient" << std::endl;
+#endif
+    count++;
+    if (count % 10 == 0 && !printed_rate) {
+      auto elapsed_ms = get_time_ms() - start_time;
+      float elapsed_sec = elapsed_ms / 1000.0;
+      if (elapsed_sec > (2 * 60)) {
+        printed_rate = true;
+        std::cout << "Update rate/sec last 2 mins: " << (1.0 * count / elapsed_sec) << std::endl;
+      }
+    }
+  }
+}
+
+} // namespace cirrus
+
---
+++ tests/test_parameter_server/parameter_server_test.cpp
@@ -0,0 +1,166 @@
+#include <Utils.h>
+#include <Configuration.h>
+#include <Tasks.h>
+#include <config.h>
+
+#include <stdlib.h>
+#include <cstdint>
+#include <string>
+
+#include <gflags/gflags.h>
+
+DEFINE_int64(nworkers, -1, "number of workers");
+DEFINE_int64(rank, -1, "rank");
+DEFINE_string(config, "", "config");
+DEFINE_string(ps_ip, PS_IP, "parameter server ip");
+DEFINE_int64(ps_port, PS_PORT, "parameter server port");
+
+static const uint64_t GB = (1024*1024*1024);
+static const uint32_t SIZE = 1;
+
+void run_tasks(int rank, int nworkers,
+    int batch_size, const cirrus::Configuration& config,
+    const std::string& ps_ip,
+    uint64_t ps_port) {
+
+  std::cout << "Run tasks rank: " << rank << std::endl;
+  int features_per_sample = config.get_num_features();
+  int samples_per_batch = config.get_minibatch_size();
+
+  if (rank == PERFORMANCE_LAMBDA_RANK) {
+    cirrus::PerformanceLambdaTask lt(features_per_sample,
+        batch_size, samples_per_batch, features_per_sample,
+        nworkers, rank, ps_ip, ps_port);
+    lt.run(config);
+    cirrus::sleep_forever();
+  } else if (rank == PS_SPARSE_SERVER_TASK_RANK) {
+    cirrus::PSSparseServerTask st((1 << config.get_model_bits()) + 1,
+        batch_size, samples_per_batch, features_per_sample,
+        nworkers, rank, ps_ip, ps_port);
+    st.run(config);
+  } else if (rank >= WORKERS_BASE && rank < WORKERS_BASE + nworkers) {
+    /**
+     * Worker tasks run here
+     * Number of tasks is determined by the value of nworkers
+     */
+    if (config.get_model_type() == cirrus::Configuration::LOGISTICREGRESSION) {
+      cirrus::LogisticSparseTaskS3Test lt(features_per_sample,
+          batch_size, samples_per_batch, features_per_sample,
+          nworkers, rank, ps_ip, ps_port);
+      lt.run(config, rank - WORKERS_BASE);
+    } else if (config.get_model_type()
+            == cirrus::Configuration::COLLABORATIVE_FILTERING) {
+      cirrus::MFNetflixTask lt(0,
+          batch_size, samples_per_batch, features_per_sample,
+          nworkers, rank, ps_ip, ps_port);
+      lt.run(config, rank - WORKERS_BASE);
+    } else {
+      exit(-1);
+    }
+  /**
+    * SPARSE tasks
+    */
+  } else if (rank == ERROR_SPARSE_TASK_RANK) {
+    cirrus::ErrorSparseTaskTest et((1 << config.get_model_bits()),
+        batch_size, samples_per_batch, features_per_sample,
+        nworkers, rank, ps_ip, ps_port);
+    et.run(config);
+    cirrus::sleep_forever();
+  } else if (rank == LOADING_SPARSE_TASK_RANK) {
+    if (config.get_model_type() == cirrus::Configuration::LOGISTICREGRESSION) {
+      cirrus::LoadingSparseTaskS3 lt((1 << config.get_model_bits()),
+          batch_size, samples_per_batch, features_per_sample,
+          nworkers, rank, ps_ip, ps_port);
+      lt.run(config);
+    } else if (config.get_model_type() ==
+            cirrus::Configuration::COLLABORATIVE_FILTERING) {
+      cirrus::LoadingNetflixTask lt(0,
+          batch_size, samples_per_batch, features_per_sample,
+          nworkers, rank, ps_ip, ps_port);
+      lt.run(config);
+    } else {
+      exit(-1);
+    }
+  } else {
+    throw std::runtime_error("Wrong task rank: " + std::to_string(rank));
+  }
+}
+
+void print_arguments() {
+  // nworkers is the number of processes computing gradients
+  // rank starts at 0
+  std::cout << "./parameter_server --config config_file "
+      << "--nworkers nworkers --rank rank [--ps_ip ps_ip] [--ps_port ps_port]"
+      << std::endl
+      << " RANKS:" << std::endl
+      << "0: load task" << std::endl
+      << "1: parameter server" << std::endl
+      << "2: error task" << std::endl
+      << "3: worker task" << std::endl
+      << std::endl;
+}
+
+cirrus::Configuration load_configuration(const std::string& config_path) {
+  cirrus::Configuration config;
+  std::cout << "Loading configuration"
+    << std::endl;
+  config.read(config_path);
+  std::cout << "Configuration read"
+    << std::endl;
+  config.check();
+  return config;
+}
+
+void print_hostname() {
+  char name[200];
+  gethostname(name, 200);
+  std::cout << "MPI multi task test running on hostname: " << name
+    << std::endl;
+}
+
+void check_arguments() {
+  if (FLAGS_nworkers == -1 || FLAGS_rank == -1 || FLAGS_config == "") {
+    print_arguments();
+    throw std::runtime_error("Some flags not specified");
+  }
+}
+
+int main(int argc, char** argv) {
+  std::cout << "Starting parameter server" << std::endl;
+
+  print_hostname();
+
+  gflags::ParseCommandLineFlags(&argc, &argv, true);
+  check_arguments();
+
+  int nworkers = FLAGS_nworkers;
+  std::cout << "Running parameter server with: "
+    << nworkers << " workers"
+    << std::endl;
+
+  int rank = FLAGS_rank;
+  std::cout << "Running parameter server with: "
+    << rank << " rank"
+    << std::endl;
+
+  auto config = load_configuration(FLAGS_config);
+  config.print();
+
+  // from config we get
+  int batch_size = config.get_minibatch_size() * config.get_num_features();
+
+  std::cout
+    << "samples_per_batch: " << config.get_minibatch_size()
+    << " features_per_sample: " << config.get_num_features()
+    << " batch_size: " << config.get_minibatch_size()
+    << std::endl;
+
+  // call the right task for this process
+  std::cout << "Running task" << std::endl;
+  run_tasks(rank, nworkers, batch_size, config, FLAGS_ps_ip, FLAGS_ps_port);
+
+  std::cout << "Test successful" << std::endl;
+
+  return 0;
+}
+
